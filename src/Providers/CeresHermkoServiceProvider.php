bb83467e7dad4dbcc14f4116abd5f58425592237
werk38 <info@werk38.de>
2018-06-26 13:01:42 +0200

    redo
    redo

--- src/Providers/CeresHermkoServiceProvider.php
+++ src/Providers/CeresHermkoServiceProvider.php
@@ -4,121 +4,266 @@ namespace CeresHermko\Providers;
 
 use Ceres\Caching\NavigationCacheSettings;
 use Ceres\Caching\SideNavigationCacheSettings;
-use Ceres\Config\CeresConfig;
-use Ceres\Contexts\CategoryContext;
-use Ceres\Contexts\CategoryItemContext;
-use Ceres\Contexts\GlobalContext;
-use Ceres\Contexts\ItemSearchContext;
-use Ceres\Contexts\ItemWishListContext;
-use Ceres\Contexts\OrderConfirmationContext;
-use Ceres\Contexts\OrderReturnContext;
-use Ceres\Contexts\PasswordResetContext;
-use Ceres\Contexts\SingleItemContext;
-use Ceres\Extensions\TwigStyleScriptTagFilter;
-use IO\Extensions\Functions\Partial;
-use IO\Helper\CategoryKey;
-use IO\Helper\CategoryMap;
-use IO\Helper\TemplateContainer;
 use IO\Services\ContentCaching\Services\Container;
-use IO\Services\ItemSearch\Helper\ResultFieldTemplate;
 use Plenty\Plugin\ServiceProvider;
-use Plenty\Plugin\Templates\Twig;
 use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\Templates\Twig;
+use IO\Helper\TemplateContainer;
+use IO\Helper\ComponentContainer;
+use IO\Extensions\Functions\Partial;
 use Plenty\Plugin\ConfigRepository;
 
+
 /**
- * Class TemplateServiceProvider
- * @package Ceres\Providers
+ * Class CeresHermkoServiceProvider
+ * @package CeresHermko\Providers
  */
 class CeresHermkoServiceProvider extends ServiceProvider
 {
-    const EVENT_LISTENER_PRIORITY = 100;
+    const PRIORITY = 0;
 
-    private static $templateKeyToViewMap =
-    [
-        'tpl.home'                      => ['Homepage.Homepage',                    GlobalContext::class],     // provide template to use for homepage
-        'tpl.category.content'          => ['Category.Content.CategoryContent',     CategoryContext::class],   // provide template to use for content categories
-        'tpl.category.item'             => ['Category.Item.CategoryItem',           CategoryItemContext::class],          // provide template to use for item categories
-        'tpl.category.blog'             => ['PageDesign.PageDesign',                GlobalContext::class],               // provide template to use for blog categories
-        'tpl.category.container'        => ['PageDesign.PageDesign',                GlobalContext::class],               // provide template to use for container categories
-        'tpl.item'                      => ['Item.SingleItemWrapper',               SingleItemContext::class],                 // provide template to use for single items
-        'tpl.basket'                    => ['Basket.Basket',                        GlobalContext::class],                       // provide template to use for basket
-        'tpl.checkout'                  => ['Checkout.CheckoutView',                GlobalContext::class],               // provide template to use for checkout
-        'tpl.my-account'                => ['MyAccount.MyAccount',                  GlobalContext::class],                 // provide template to use for my-account
-        'tpl.confirmation'              => ['Checkout.OrderConfirmation',           OrderConfirmationContext::class],          // provide template to use for confirmation
-        'tpl.login'                     => ['Customer.Login',                       GlobalContext::class],                      // provide template to use for login
-        'tpl.register'                  => ['Customer.Register',                    GlobalContext::class],                   // provide template to use for register
-        'tpl.guest'                     => ['Customer.Guest',                       GlobalContext::class],                      // provide template to use for guest
-        'tpl.password-reset'            => ['Customer.ResetPassword',               PasswordResetContext::class],              // provide template to use for password-reset
-        'tpl.contact'                   => ['Customer.Contact',                     GlobalContext::class],                    // provide template to use for contact
-        'tpl.search'                    => ['ItemList.ItemListView',                ItemSearchContext::class],               // provide template to use for item search
-        'tpl.wish-list'                 => ['WishList.WishListView',                ItemWishListContext::class],               // provide template to use for wishlist
-        'tpl.order.return'              => ['OrderReturn.OrderReturnView',          OrderReturnContext::class],         // provide template to use for order return
-        'tpl.order.return.confirmation' => ['OrderReturn.OrderReturnConfirmation',  GlobalContext::class], // provide template to use for order return confirmation
-        'tpl.cancellation-rights'       => ['StaticPages.CancellationRights',       GlobalContext::class],      // provide template to use for cancellation rights
-        'tpl.cancellation-form'         => ['StaticPages.CancellationForm',         GlobalContext::class],        // provide template to use for cancellation form
-        'tpl.legal-disclosure'          => ['StaticPages.LegalDisclosure',          GlobalContext::class],         // provide template to use for legal disclosure
-        'tpl.privacy-policy'            => ['StaticPages.PrivacyPolicy',            GlobalContext::class],           // provide template to use for privacy policy
-        'tpl.terms-conditions'          => ['StaticPages.TermsAndConditions',       GlobalContext::class],      // provide template to use for terms and conditions
-        'tpl.item-not-found'            => ['StaticPages.ItemNotFound',             GlobalContext::class],            // provide template to use for item not found
-        'tpl.page-not-found'            => ['StaticPages.PageNotFound',             GlobalContext::class]             // provide template to use for page not found
-    ];
+    public function register()
+    {
 
-    public function register(){
-        $this->getApplication()->singleton( CeresConfig::class );
     }
 
-    public function boot(Twig $twig, Dispatcher $eventDispatcher, ConfigRepository $config)
+    public function boot(Twig $twig, Dispatcher $dispatcher, ConfigRepository $config)
     {
-        // Register Twig String Loader to use function: template_from_string
-        $twig->addExtension('Twig_Extension_StringLoader');
-        $twig->addExtension(TwigStyleScriptTagFilter::class);
 
-        $eventDispatcher->listen('IO.tpl.*', function (TemplateContainer $templateContainer, $templateData = []) {
-            if ( !$templateContainer->hasTemplate() )
-            {
-                $templateName = self::$templateKeyToViewMap[$templateContainer->getTemplateKey()][0];
-                $templateContainer->setTemplate('CeresHermko::' . $templateName);
-            }
-        }, self::EVENT_LISTENER_PRIORITY);
+        $enabledOverrides = explode(", ", $config->get("CeresHermko.templates.override"));
 
-        $eventDispatcher->listen('IO.ctx.*', function (TemplateContainer $templateContainer, $templateData = []) {
-            $templateContextClass = self::$templateKeyToViewMap[$templateContainer->getTemplateKey()][1];
-            if(!strlen($templateContextClass))
-            {
-                $templateContextClass = GlobalContext::class;
-            }
-            $templateContainer->setContext( $templateContextClass );
-        }, self::EVENT_LISTENER_PRIORITY);
-
-        $eventDispatcher->listen( 'IO.ResultFields.*', function(ResultFieldTemplate $templateContainer) {
-            $templateContainer->setTemplates([
-                ResultFieldTemplate::TEMPLATE_LIST_ITEM     => 'CeresHermko::ResultFields.ListItem',
-                ResultFieldTemplate::TEMPLATE_SINGLE_ITEM   => 'CeresHermko::ResultFields.SingleItem',
-                ResultFieldTemplate::TEMPLATE_BASKET_ITEM   => 'CeresHermko::ResultFields.BasketItem'
-            ]);
-        }, self::EVENT_LISTENER_PRIORITY);
-
-        // provide mapped category IDs - DEPRECATED?
-        $eventDispatcher->listen('init.categories', function (CategoryMap $categoryMap) use (&$config) {
-            $categoryMap->setCategoryMap(array(
-                CategoryKey::HOME => $config->get("Ceres.global.category.home"),
-                CategoryKey::PAGE_NOT_FOUND => $config->get("Ceres.global.category.page_not_found"),
-                CategoryKey::ITEM_NOT_FOUND => $config->get("Ceres.global.category.item_not_found")
-            ));
-
-        }, self::EVENT_LISTENER_PRIORITY);
-
-        $eventDispatcher->listen('IO.init.templates', function (Partial $partial){
-
+        // Override partials
+        $dispatcher->listen('IO.init.templates', function (Partial $partial) use ($enabledOverrides)
+        {
             pluginApp(Container::class)->register('CeresHermko::PageDesign.Partials.Header.NavigationList.twig', NavigationCacheSettings::class);
             pluginApp(Container::class)->register('CeresHermko::PageDesign.Partials.Header.SideNavigation.twig', SideNavigationCacheSettings::class);
 
-            $partial->set('head', 'CeresHermko::PageDesign.Partials.Head');
-            $partial->set('header', 'CeresHermko::PageDesign.Partials.Header.Header');
-            $partial->set('footer', 'CeresHermko::PageDesign.Partials.Footer');
-            $partial->set('page-design', 'CeresHermko::PageDesign.PageDesign');
+            $partial->set('head', 'Ceres::PageDesign.Partials.Head');
+            $partial->set('header', 'Ceres::PageDesign.Partials.Header.Header');
+            $partial->set('page-design', 'Ceres::PageDesign.PageDesign');
+            $partial->set('footer', 'Ceres::PageDesign.Partials.Footer');
 
-        }, self::EVENT_LISTENER_PRIORITY);
+            if (in_array("head", $enabledOverrides) || in_array("all", $enabledOverrides))
+            {
+                $partial->set('head', 'CeresHermko::PageDesign.Partials.Head');
+            }
+
+            if (in_array("header", $enabledOverrides) || in_array("all", $enabledOverrides))
+            {
+                $partial->set('header', 'CeresHermko::PageDesign.Partials.Header.Header');
+            }
+
+            if (in_array("page_design", $enabledOverrides) || in_array("all", $enabledOverrides))
+            {
+                $partial->set('page-design', 'CeresHermko::PageDesign.PageDesign');
+            }
+
+            if (in_array("footer", $enabledOverrides) || in_array("all", $enabledOverrides))
+            {
+                $partial->set('footer', 'CeresHermko::PageDesign.Partials.Footer');
+            }
+
+            return false;
+        }, self::PRIORITY);
+
+        // Override homepage
+        if (in_array("homepage", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.home', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::Homepage.Homepage');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override template for content categories
+        if (in_array("category_content", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.category.content', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::Category.Content.CategoryContent');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override template for item categories
+        if (in_array("category_item", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.category.item', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::Category.Item.CategoryItem');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        $dispatcher->listen('IO.Component.Import', function(ComponentContainer $container){
+            if( $container->getOriginComponentTemplate() == 'Ceres::ItemList.Components.ItemList')
+            {
+               $container->setNewComponentTemplate('CeresHermko::ItemList.Components.ItemList');
+            }
+          }, 0);
+
+        // Override shopping cart
+        if (in_array("basket", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.basket', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::Basket.Basket');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override checkout
+        if (in_array("checkout", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.checkout', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::Checkout.CheckoutView');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override order confirmation page
+        if (in_array("order_confirmation", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.confirmation', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::Checkout.OrderConfirmation');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override login page
+        if (in_array("login", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.login', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::Customer.Login');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override register page
+        if (in_array("register", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.register', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::Customer.Register');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override single item page
+        if (in_array("item", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.item', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::Item.SingleItemWrapper');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override category view
+        if (in_array("category_view", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.search', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::ItemList.ItemListView');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        $dispatcher->listen('IO.Component.Import', function(ComponentContainer $container){
+             if( $container->getOriginComponentTemplate() == 'Ceres::ItemList.Components.CategoryItem')
+             {
+                $container->setNewComponentTemplate('CeresHermko::ItemList.Components.CategoryItem');
+             }
+           }, 0);
+        // Override my account
+        if (in_array("my_account", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.my-account', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::MyAccount.MyAccount');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override cancellation rights
+        if (in_array("cancellation_rights", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.cancellation-rights', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::StaticPages.CancellationRights');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override legal disclosure
+        if (in_array("legal_disclosure", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.legal-disclosure', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::StaticPages.LegalDisclosure');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override privacy policy
+        if (in_array("privacy_policy", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.privacy-policy', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::StaticPages.PrivacyPolicy');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override terms and conditions
+        if (in_array("terms_conditions", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.terms-conditions', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::StaticPages.TermsAndConditions');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override item not found page
+        if (in_array("item_not_found", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.item-not-found', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::StaticPages.ItemNotFound');
+                return false;
+            }, self::PRIORITY);
+        }
+
+        // Override page not found page
+        if (in_array("page_not_found", $enabledOverrides) || in_array("all", $enabledOverrides))
+        {
+
+            $dispatcher->listen('IO.tpl.page-not-found', function (TemplateContainer $container)
+            {
+                $container->setTemplate('CeresHermko::StaticPages.PageNotFound');
+                return false;
+            }, self::PRIORITY);
+        }
     }
 }
